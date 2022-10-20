dfbf6f10a205fa3dfd1d56925684d17307ebbc39
Robsonsud1983 <115170900+Robsonsud1983@users.noreply.github.com>
2022-10-20 00:44:14 -0300

    Add files via upload

---
+++ Casos de testes.py
@@ -0,0 +1,7 @@
+class MenuItem:
+    menuItem = []
+
+    def __init__(self, id : int, nome : str, preco : float) -> None:
+        self.id = id
+        self.nome = nome
+        self.preco = preco
